e3decafda99319178beeff37a5bd077b27191ef7
dwhr-pi <74153496+dwhr-pi@users.noreply.github.com>
2022-01-01 19:08:28 +0100

    Update zenbot.sh

--- zenbot.sh
+++ zenbot.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-echo "Starte Darwin"
+echo "Starte Zenbot mit 3072 MB Arbeitsspeicher"
 env node  --trace-warnings --max-old-space-size="3072" zenbot.js $@
-echo "Darwin gestartet. 4096"
+echo "Zenbot gestartet. 3072 MB"
